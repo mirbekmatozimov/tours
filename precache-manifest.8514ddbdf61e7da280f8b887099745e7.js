92dfbb99db9264df7d210d9c9ffddd323fcf669b
mirbek <mirbek.25.52@gmail.com>
2021-06-06 15:32:10 +0600

    Updates

---
+++ precache-manifest.8514ddbdf61e7da280f8b887099745e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68dc6e93e5528dec6a39371fcaa90bd0",
+    "url": "/tours/index.html"
+  },
+  {
+    "revision": "a835201385dbb3de74a7",
+    "url": "/tours/static/css/main.2ee3d264.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a835201385dbb3de74a7",
+    "url": "/tours/static/js/main.3075e8a3.chunk.js"
+  },
+  {
+    "revision": "cabdc42b426759795a4b",
+    "url": "/tours/static/js/runtime-main.3b7494d8.js"
+  }
+]);
